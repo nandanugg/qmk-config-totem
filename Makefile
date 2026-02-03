fe4e9688668ad437b3cd8955dc0a3e123bf851eb
nandanugg <nandanugg@gmail.com>
2026-02-03 15:36:34 +0700

    update layout

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
 init:
 	sudo ln -s $(pwd)/totem ~/qmk_firmware/keyboards/
 	qmk config user.keyboard=totem
-compile-l:
-	qmk compile -km default
-compile-r:
+compile:
 	qmk compile -km default
 flash-l:
 	qmk flash -km default -bl uf2-split-left
--- totem/keymaps/default/keymap.c
+++ totem/keymaps/default/keymap.c
@@ -120,23 +120,25 @@ enum custom_keycodes {
 // ▝▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▘
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-		          KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,      KC_I,    KC_O,      KC_P,
-           	          KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,      KC_K,    KC_L,      KC_SCLN,
-                KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     MT(MOD_LGUI, KC_B),      KC_N,     KC_M,      KC_COMM, KC_DOT,    KC_SLSH,   KC_ENT,
-                                              KC_LCTL, KC_LALT,  KC_SPC,    KC_SPC,   MO(1),     MO(2)
+[0] = LAYOUT(
+              KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+              KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    MT(MOD_LGUI, KC_B),       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+                                 KC_LCTL, KC_LALT, KC_SPC,            KC_SPC,  MO(1),   MO(2)
     ),
+
     [1] = LAYOUT(
-		          KC_ESC,   KC_1,     KC_2,     KC_3,     KC_0,      KC_LPRN,  KC_RPRN,   KC_LBRC, KC_RBRC,   KC_BSPC,
-           	          KC_TAB,   KC_4,     KC_5,     KC_6,     _______,   KC_LEFT,  KC_DOWN,   KC_UP,   KC_RIGHT,  KC_QUOT,
-        _______,          _______,  KC_7,     KC_8,     KC_9,     _______,   _______,  _______,   _______, _______,   _______,   KC_BSLS,
-                                              _______,  _______,  _______,   _______,  _______,   _______
+              KC_ESC,  KC_1,    KC_2,    KC_3,    KC_0,                      KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_BSPC,
+              KC_TAB,  KC_4,    KC_5,    KC_6,    _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_QUOT,
+      _______, _______, KC_7,    KC_8,    KC_9,    _______,                   _______, _______, _______, _______, _______, KC_BSLS,
+                                 _______, _______, _______,           _______, _______, _______
     ),
+
     [2] = LAYOUT(
-		          KC_GRAVE, KC_F1,    KC_F2,    KC_F3,    KC_F10,    KC_MPLY,  KC_MSTP,   KC_MINUS,          KC_EQUAL,         KC_DEL,
-           	          _______,  KC_F4,    KC_F5,    KC_F6,    KC_F11,    _______,  _______,   KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP,  _______,
-        _______,          _______,  KC_F7,    KC_F8,    KC_F9,    KC_F12,    KC_MNXT,  KC_MPRV,   _______,           _______,          _______,   _______,
-                                              _______,  _______,  _______,   _______,  _______,   _______
+              KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F10,                    KC_MPLY, KC_MSTP, KC_MINUS, KC_EQL,  KC_DEL,
+              _______, KC_F4,   KC_F5,   KC_F6,   KC_F11,                    KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______,
+      _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F12,                    _______, _______ _______,  _______, _______, _______,
+                                 _______, _______, _______,           _______, _______, _______
     )
     // [3] = LAYOUT(
     //         _______,  _______,  _______,  _______,  _______,    _______, _______,   _______, _______,  _______,
