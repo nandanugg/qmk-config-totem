3bfbfb57d3b754457fc13353f838e90359383e0b
nandanugg <nandanugg@gmail.com>
2026-02-11 08:26:08 +0700

    update timeout

--- totem/keymaps/default/config.h
+++ totem/keymaps/default/config.h
@@ -11,6 +11,7 @@
 #define SERIAL_USART_PIN_SWAP     // Swap TX and RX pins if keyboard is master halve.
 #define EE_HANDS // Split handedness via eeprom
 #define OS_DETECTION_SINGLE_REPORT
+#define TAPPING_TERM 500
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
