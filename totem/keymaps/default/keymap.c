7f72147db79dd7c2040c11279d977a68fa33c37f
nandanugg <nandanugg@gmail.com>
2026-04-08 01:16:55 +0700

    Update keymap: remove VER_CTRL and use standard KC_LCTL

--- totem/keymaps/default/keymap.c
+++ totem/keymaps/default/keymap.c
@@ -22,7 +22,6 @@ enum custom_keycodes {
 	VER_ALT,
 	VER_ALT_MOD,
 	VER_LSFT_L3,
-	VER_CTRL,
 	VER_SPACE,
 };
 
@@ -61,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
               KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
       VER_LSFT_L3, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-                                 VER_CTRL, VER_ALT, VER_SPACE,         KC_SPC,  MO(1),   MO(2)
+                                 KC_LCTL, VER_ALT, VER_SPACE,         KC_SPC,  MO(1),   MO(2)
     ),
 
     [1] = LAYOUT(
@@ -94,7 +93,6 @@ static bool lsft_active = false;
 static bool ver_alt_active = false;
 static bool ver_alt_tab_swapped = false;
 static bool ver_alt_home_end_swapped = false;
-static bool ver_ctrl_active = false;
 static bool ver_alt_mod_active = false;
 static bool ver_alt_mod_ctrl_swapped = false;
 static uint16_t ver_space_timer = 0;
@@ -223,20 +221,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                ver_alt_mod_ctrl_swapped = false;
             } else {
                unregister_code(ver_alt_mod_keycode());
-            }
-         }
-         return false;
-      case VER_CTRL:
-         if (record->event.pressed) {
-            ver_ctrl_active = true;
-            register_code(KC_LCTL);
-         } else {
-            ver_ctrl_active = false;
-            unregister_code(KC_LCTL);
-         }
-         return false;
-      case VER_SPACE:
-         if (record->event.pressed) {
+             }
+          }
+          return false;
+       case VER_SPACE:
+          if (record->event.pressed) {
             ver_space_timer = timer_read();
             register_code(KC_LALT);
          } else {
